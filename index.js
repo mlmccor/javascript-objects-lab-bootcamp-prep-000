f0b45fe9690ae98b08c3dc8dc1d81b14fd2a2fe4
Matthew McCormack <mmccormack1182@gmail.com>
2018-04-15 22:24:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, {[key]:value})
 }
+function 
